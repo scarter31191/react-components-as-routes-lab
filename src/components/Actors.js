7113443da9fff621661ffd0e09cb817b05437cd8
scarter31191 <scarter31191@gmaile.com>
2021-05-23 15:48:13 -0400

    commit tree 31ffe3f47208aaf146c67b6387e6871677abb657

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -4,7 +4,17 @@ import { actors } from '../data';
 const Actors = () => {
   return (
     <div>
-      {/*{code here}*/}
+      <h1>Actors Page</h1>
+        {actors.map((actors, index) => (
+            <div key={index}>
+              <h3>Name: {actors.name}</h3>
+                {actors.movies.map((movies, index) => (
+                  <ul key={index}>
+                    <li key={index}>{movies}</li>
+                  </ul>
+                ))}
+            </div>
+        ))}
     </div>
   );
 };
--- src/components/Directors.js
+++ src/components/Directors.js
@@ -4,7 +4,18 @@ import { directors } from '../data';
 const Directors = () => {
   return (
     <div>
-      {/*{code here}*/}
+      <h1>Directors Page</h1>
+      {directors.map((directors, index) => (
+        <div key={index}>
+            <h3> Name: {directors.name}</h3>
+            <p>Movies:</p>
+              <ul>
+                {directors.movies.map((movies, index) => (
+                  <li key={index}>{movies}</li>
+                ))}
+              </ul>
+        </div>
+      ))}
     </div>
   );
 }
